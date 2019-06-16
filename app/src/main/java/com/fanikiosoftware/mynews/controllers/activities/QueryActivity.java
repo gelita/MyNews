030e2f10bd2c3e6fae30720ad403cc8a21b84267
Angela Rosas <gelita@users.noreply.github.com>
2019-06-16 13:30:54 -0700

    adding date spinner for search query start date(not working)

--- app/src/main/java/com/fanikiosoftware/mynews/controllers/activities/QueryActivity.java
+++ app/src/main/java/com/fanikiosoftware/mynews/controllers/activities/QueryActivity.java
@@ -1,5 +1,6 @@
 package com.fanikiosoftware.mynews.controllers.activities;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +9,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fanikiosoftware.mynews.R;
+
+import java.nio.FloatBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,13 +30,15 @@ import butterknife.ButterKnife;
  */
 public class QueryActivity extends AppCompatActivity {
 
-    public String query = "";
-
+    @BindView(R.id.tvEndDateSpinner)
+    TextView tvEndDateSpinner;
+    @BindView(R.id.tvStartDateSpinner)
+    TextView tvStartDateSpinner;
     @BindView(R.id.etSearch)
     EditText etSearch;
-    @BindView(R.id.etDateStart)
+    @BindView(R.id.etDateStartLabel)
     EditText etDateStart;
-    @BindView(R.id.etDateEnd)
+    @BindView(R.id.etDateEndLabel)
     EditText etDateEnd;
     @BindView(R.id.btnSubmit)
     Button btnSubmit;
@@ -47,6 +57,10 @@ public class QueryActivity extends AppCompatActivity {
     @BindView(R.id.notificationSwitch)
     Switch notificationSwitch;
     String title;
+    private DatePickerDialog.OnDateSetListener date;
+    public String query = "";
+    private Calendar myCalendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,10 +70,53 @@ public class QueryActivity extends AppCompatActivity {
         getActivityTitle();
         setSwitch(title);
         setTitle(title);
+        setUpListeners();
+        getDate();
+    }
+
+    private void getDate() {
+        myCalendar = Calendar.getInstance();
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        tvStartDateSpinner.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void setUpListeners() {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getQuery();
+            }
+        });
+
+        tvEndDateSpinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        tvStartDateSpinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(getApplicationContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
     }
@@ -72,11 +129,10 @@ public class QueryActivity extends AppCompatActivity {
         } else if (!query.isEmpty()) {
             System.out.println("Search term: " + query);
             //confirm that at least ONE checkbox is checked or give feedback to user
-            if(!check1.isChecked() && !check2.isChecked() && !check3.isChecked() &&
-                    !check4.isChecked() && !check5.isChecked() && !check6.isChecked()){
+            if (!check1.isChecked() && !check2.isChecked() && !check3.isChecked() &&
+                    !check4.isChecked() && !check5.isChecked() && !check6.isChecked()) {
                 Toast.makeText(getApplicationContext(), R.string.checkbox_required, Toast.LENGTH_SHORT).show();
             }
-
         }
     }
 
